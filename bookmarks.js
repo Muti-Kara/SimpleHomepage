aa510393e862b7731ec7a1b0db5007fabfe4c540
Muti Kara <91342202+Muti-Kara@users.noreply.github.com>
2022-03-03 17:47:19 +0300

    Update bookmarks.js

--- bookmarks.js
+++ bookmarks.js
@@ -1,35 +1,35 @@
 // Spesifically designed for 4x3
 const bookmarks = [
   {
-    title: "Daily",
+    title: "Title1",
     links: [
-      { name: "Youtube", url: "https://youtube.com" },
-      { name: "Gmail", url: "https://mail.google.com/mail/u/0/#inbox" },
-      { name: "Translate", url: "https://translate.google.com/" },
+      { name: "Name 1-1", url: "" },
+      { name: "Name 1-2", url: "" },
+      { name: "Name 1-3", url: "" },
     ],
   },
   {
-    title: "CS",
+    title: "Title2",
     links: [
-	  { name: "Codeforces", url: "https://codeforces.com"},
-      { name: "GitHub", url: "https://github.com/Muti-Kara" },
-      { name: "Studying", url: "https://beej.us/guide/bgnet/html/" },
+      { name: "Name 2-1", url: ""},
+      { name: "Name 2-2", url: "" },
+      { name: "Name 2-3", url: "" },
     ],
   },
   {
-    title: "Project",
+    title: "Title3",
     links: [
-      { name: "Machine L.", url: "https://github.com/eclipse/deeplearning4j" },
-      { name: "Language", url: "https://github.com/mimno/Mallet" },
-      { name: "Azure", url: "https://azure.microsoft.com/tr-tr/" },
+      { name: "Name 3-1", url: "" },
+      { name: "Name 3-2", url: "" },
+      { name: "Name 3-3", url: "" },
     ],
   },
   {
-    title: "Bilkent",
+    title: "Title4",
     links: [
-      { name: "Stars", url: "https://stars.bilkent.edu.tr" },
-      { name: "Moodle", url: "https://moodle.bilkent.edu.tr/2021-2022-spring/login/index.php" },
-      { name: "WebAssign", url: "https://www.webassign.net/" },
+      { name: "Name 4-1", url: "" },
+      { name: "Name 4-2", url: ""},
+      { name: "Name 4-3", url: "" },
     ],
   },
 ];
